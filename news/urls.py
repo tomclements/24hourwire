ac5ff8327f716f2d744c8fef143ecfc644228d86
tomclements <tomclements@users.noreply.github.com>
2026-03-25 23:35:48 -0500

    Add About page and improve link explanations

--- news/urls.py
+++ news/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', TemplateView.as_view(template_name='about.html'), name='about'),
     path('terms/', TemplateView.as_view(template_name='terms.html'), name='terms'),
     path('privacy/', TemplateView.as_view(template_name='privacy.html'), name='privacy'),
 ]
